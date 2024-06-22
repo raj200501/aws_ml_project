4d68e7fa22605d89479ce2dbc1146bf9cfe7d481
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-22 15:57:16 -0700

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,8 +1,10 @@
 #include "data_preprocessing.h"
+#include "data_analysis.h"
+#include "ml_model.h"
 #include <iostream>
 
 int main() {
-    std::string filename = "data/sample_data.csv";
+    std::string filename = "../../data/sample_data.csv";
     std::vector<std::vector<float>> data = load_data(filename);
 
     std::cout << "Original data:" << std::endl;
@@ -22,6 +24,33 @@ int main() {
         }
         std::cout << std::endl;
     }
+
+    std::vector<float> feature1;
+    for (const auto &row : data) {
+        feature1.push_back(row[0]);
+    }
+    float mean = calculate_mean(feature1);
+    float std_dev = calculate_std_dev(feature1, mean);
+
+    std::cout << "Feature1 Mean: " << mean << ", Std Dev: " << std_dev << std::endl;
+
+    // Prepare data for model training
+    std::vector<std::vector<float>> X(data.size(), std::vector<float>(data[0].size() - 1));
+    std::vector<int> y(data.size());
+    for (size_t i = 0; i < data.size(); ++i) {
+        X[i] = {data[i][0], data[i][1], data[i][2]};
+        y[i] = static_cast<int>(data[i][3]);
+    }
+
+    RandomForest model(10);
+    model.fit(X, y);
+    std::vector<int> predictions = model.predict(X);
+
+    std::cout << "Predictions:" << std::endl;
+    for (const auto &pred : predictions) {
+        std::cout << pred << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
